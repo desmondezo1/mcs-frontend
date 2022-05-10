16aab4925d77f9a9bc539bc674dac5404f99311f
Desmond <desezo@gmail.com>
2022-05-10 22:49:08 +0100

    second section for home page

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,11 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import jumbotron from'../public/images/jumbotron.png'
 import downArrow from '../public/images/arrowdown.png'
+import toilet from '../public/images/toilet.png'
+import washingMachine from '../public/images/washingmachine.png'
+import houseImage from '../public/images/house.png'
+import window from '../public/images/window.png'
+import { Icon } from '@iconify/react'
 
 
 export default function Home() {
@@ -42,6 +47,51 @@ export default function Home() {
           </span>
         </div>
 
+      </div>
+      <div className={styles.distributorsSection}>
+        <div className={styles.distributorsSectionWrapper}>
+
+          <div className={styles.distImageCard}>
+            <Image src={toilet} />
+            <div>
+              <span>
+              WASHROOM
+                <Icon icon="carbon:arrow-up-right"  style={{ fontSize: '1.5rem' }}/>
+              </span>
+            </div>
+          </div>
+
+          <div className={styles.distImageCard}>
+            <Image src={washingMachine} />
+            <div>
+              <span>
+              MACCHINARI
+                <Icon icon="carbon:arrow-up-right"  style={{ fontSize: '1.5rem' }}/>
+              </span>
+            </div>
+          </div>
+
+          <div className={styles.distImageCard}>
+            <Image src={houseImage} />
+            <div>
+              <span>
+              TAPPETI ANTISCIVOLO
+                <Icon icon="carbon:arrow-up-right"  style={{ fontSize: '1.5rem' }}/>
+              </span>
+            </div>
+          </div>
+
+          <div className={styles.distImageCard}>
+            <Image src={window} />
+            <div>
+              <span>
+              MONITORAGGIO
+                <Icon icon="carbon:arrow-up-right"  style={{ fontSize: '1.5rem' }}/>
+              </span>
+            </div>
+          </div>
+
+        </div>
       </div>
     </main>
     </>
