b193a446d35a9818660a7201567df04628cbc226
Desmond <desezo@gmail.com>
2022-05-19 10:41:57 +0100

    mod confid=g routes

--- config/routeConfig.js
+++ config/routeConfig.js
@@ -1,6 +1,6 @@
 import nextConfig from "../next.config";
-// const baseUrl = nextConfig.env.ENVIROMENT === "local" ? "http://127.0.0.1:8000/api" : nextConfig.env.BACKEND_API_BASE_URL ;
-let baseUrl = "http://127.0.0.1:8000/api";
+const baseUrl = nextConfig.env.ENVIROMENT === "local" ? "http://127.0.0.1:8000/api" : nextConfig.env.BACKEND_API_BASE_URL ;
+// let baseUrl = "http://127.0.0.1:8000/api";
 
 export default {
     createProduct : baseUrl + "/admin/products/create",
