875de1b23b048b2f0be27353f67ccfc07a1df5c4
philzace1 <unknownumeh@gmail.com>
2022-05-24 00:11:53 +0100

    fix login css issue

--- components/molecules/Login/LoginForm.js
+++ components/molecules/Login/LoginForm.js
@@ -1,120 +1,118 @@
 import React from "react";
 
-// import Input from "../../atoms/Input";
-import Cookies from 'js-cookie'
+import Input from "../../atoms/Input";
+import Cookies from "js-cookie";
 import style from "../../../styles/molecule.module.css";
 import Button from "../../atoms/Buttons";
 import Checkbox from "../../atoms/Checkbox";
 import { useState } from "react";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 import routeConfig from "../../../config/routeConfig";
-import { useFormik, Field,FormikProvider } from 'formik';
+import { useFormik, Field, FormikProvider } from "formik";
 import axios from "axios";
 
-
 function LoginForm(props) {
-  
   // let [email, setEmail] = useState('');
   // let [password, setPassword] = useState('');
 
-  const router = useRouter()
+  const router = useRouter();
 
   function handleLogin(resp) {
-
-    if(!resp.data.access_token){
+    if (!resp.data.access_token) {
       return;
-    } 
-    // set cookie 
+    }
+    // set cookie
 
-    Cookies.set('token',resp.data.access_token);
-    // set storage 
+    Cookies.set("token", resp.data.access_token);
+    // set storage
     localStorage.setItem("token", resp.data.access_token);
-    router.push('/admin/overview');
-   
-
+    router.push("/admin/overview");
   }
-  
 
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
 
-    const formik = useFormik({
-      initialValues: {
-        email: '',
-        password: '',
-      },
+    onSubmit: async (values) => {
+      const loginUrl = routeConfig.login;
+      console.log({ loginUrl });
+      let formD = await values;
+      const axiosConfig = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
 
-      onSubmit: async values => {
-        const loginUrl = routeConfig.login;
-        console.log({loginUrl});
-        let formD = await values;
-        const axiosConfig = {
-          headers: {
-              'Content-Type': 'application/json',
-          }
-        }
-      
-
-        let ax = await axios.post(
-            loginUrl,
-            formD,
-            axiosConfig
-        ).then(result =>{
-
-
+      let ax = await axios.post(loginUrl, formD, axiosConfig).then((result) => {
         console.log(result);
         handleLogin(result);
-      
-      })
+      });
+    },
+  });
 
-      },
-    });
-
-  const { errors, touched, values, isSubmitting, handleSubmit, getFieldProps,handleChange } = formik;
-
+  const {
+    errors,
+    touched,
+    values,
+    isSubmitting,
+    handleSubmit,
+    getFieldProps,
+    handleChange,
+  } = formik;
 
   return (
     <div className={style.login_form}>
       <h4 className="underlined_text">ACCEDI</h4>
       <FormikProvider value={formik}>
-      <form onSubmit={handleSubmit} >
-        <input
-         id="email"
-         name="email"
-         type="email"
-         onChange={handleChange}
-         value={values.email}
-         placeholder="E-MAIL*" 
-        />
+        <form
+          onSubmit={handleSubmit}
+          className={style.login_form_child_component}
+        >
+          <Input
+            id="email"
+            name="email"
+            type="email"
+            onChange={handleChange}
+            value={values.email}
+            placeholder="E-MAIL*"
+          />
 
-        <input 
-         id="password"
-         name="password"
-         type="password"
-         onChange={handleChange}
-         value={values.password}
-         placeholder="PASSWORD*" />
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            onChange={handleChange}
+            value={values.password}
+            placeholder="PASSWORD*"
+          />
 
+          <Button
+            style={{
+              width: "100%",
+              justifyContent: "center",
+            }}
+            type="submit"
+            size="100%"
+            alignText="center"
+            color={"primary"}
+          >
+            LOG IN
+          </Button>
+        </form>
         <Button
           style={{
             width: "100%",
-            justifyContent: "center",
+            alignItems: "center",
           }}
-
-          type="submit"
+          color="secondary"
+          alignText="center"
+          size={"100%"}
         >
-        LOG IN
-      </Button>
-      </form>
-      <Button
-        style={{
-          width: "100%",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-        color="secondary"
-      >
-        PASSWORD DIMENTICATA?
-      </Button>
-      <Checkbox id={"ricordami"} label="Ricordami" />
+          PASSWORD DIMENTICATA?
+        </Button>
+        <Checkbox id={"ricordami"} label="Ricordami" />
       </FormikProvider>
     </div>
   );
