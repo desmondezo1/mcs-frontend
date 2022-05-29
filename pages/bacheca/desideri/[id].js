d892889f2cf9c6a689234d225f25f08d3e9c4142
JoshX [MacBookPro] <joshua.osagie01@gmail.com>
2022-05-29 18:25:47 +0100

    [WIP]

--- pages/bacheca/desideri/[id].js
+++ pages/bacheca/desideri/[id].js
@@ -1,8 +1,12 @@
+import Error from "next/error";
 import Orders from "../../../components/orders";
 import Sidebar from "../../../components/sidebar";
-import Cok from 'cookie'
+import Cok from "cookie";
 
-const Desideri = ({ wishlists }) => {
+const Desideri = ({ errorCode, wishlists }) => {
+  if (errorCode) {
+    return <Error statusCode={errorCode} />;
+  }
   return (
     <div className=" pt-4 pb-[5em] md:px-5 lg:px-[4em]">
       <div className="flex flex-wrap  justify-between  px-4">
@@ -21,22 +25,32 @@ const Desideri = ({ wishlists }) => {
 
 export default Desideri;
 
-export async function getServerSideProps({req, params }) {
-
-  
-  let cook = Cok.parse( req.headers.cookie ) || '';
+export async function getServerSideProps({ req, params }) {
+  let cook = Cok.parse(req.headers.cookie) || "";
   let token = cook.token;
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  }
+  
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}user/${params?.id}/wishlist`,
     {
-    method: "GET",
-    headers: {
-      "Content-type": "application/json;charset=UTF-8",
-      'Authorization': `Bearer ${token}`,
-      }
-    }   
+      method: "GET",
+      headers: {
+        "Content-type": "application/json;charset=UTF-8",
+        Authorization: `Bearer ${token}`,
+      },
+    }
   );
+  const errorCode = res.ok ? false : res.statusCode;
+  
   const orders = await res.json();
 
-  return { props: { orders,params } };
+  return { props: { orders, params, errorCode } };
 }
