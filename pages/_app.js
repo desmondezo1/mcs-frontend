a9aff572c2d84025143d23841a08035da8cda831
philzace1 <unknownumeh@gmail.com>
2022-07-12 03:56:44 +0100

    Login and Protected Route

--- pages/_app.js
+++ pages/_app.js
@@ -1,52 +1,84 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "../styles/globals.css";
 import Head from "next/head";
-import Script from "next/script"
-import {Provider} from "react-redux";
+import Script from "next/script";
+import { Provider } from "react-redux";
 import store from "../stores";
-import '../styles/globals.css'
+import "../styles/globals.css";
 import Header from "../components/layout/header";
 import MepaHeader from "../components/layout/mepaHeader";
-import Footer from "../components/layout/footer"
+import Footer from "../components/layout/footer";
 import { useRouter } from "next/router";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   return (
     <>
-    <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-        crosOrigin="anonymous" />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+          crosOrigin="anonymous"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Arimo:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Open+Sans:ital,wght@0,300;0,400;1,300;1,400&display=swap" rel="stylesheet" />
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Almarai:wght@300;400;700;800&display=swap" rel="stylesheet"/>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Arimo:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Open+Sans:ital,wght@0,300;0,400;1,300;1,400&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Almarai:wght@300;400;700;800&display=swap"
+          rel="stylesheet"
+        />
         <title>MCS</title>
-        <link rel="apple-touch-icon" sizes="180x180" href="/images/metaAssets/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/images/metaAssets/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/images/metaAssets/favicon-16x16.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/metaAssets/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/metaAssets/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/metaAssets/favicon-16x16.png"
+        />
         <link rel="manifest" href="/images/metaAssets/site.webmanifest" />
         <meta name="description" content="MCS Official Website" />
         <link rel="icon" href="/images/metaAssets/favicon.ico" />
-    </Head>
-    <Script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-      crossorigin="anonymous" 
+      </Head>
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+        crossorigin="anonymous"
       />
       <Provider store={store}>
-        {router.asPath == "/mepa" ? (<MepaHeader/>) :(<Header />)}
-      <Component {...pageProps} />
+        {router.asPath == "/mepa" ? <MepaHeader /> : <Header />}
+        <Component {...pageProps} />
+        <ToastContainer />
       </Provider>
       <Footer />
     </>
-  
-  )
+  );
 }
 
 export default MyApp;
--- pages/shop/cart.js
+++ pages/shop/cart.js
@@ -1,161 +1,174 @@
-import { useState, useEffect } from 'react';
-import Link from 'next/link'
-import Image from 'next/image';
-import { Icon } from '@iconify/react';
-import { useSelector, useDispatch } from 'react-redux';
-import { updateTotalPrice } from '../../stores/mySlice';
-import TableBody from '../../components/checkout/table';
-import { useRouter } from 'next/router';
-import useStore from '../../stores/zustandStore';
-
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { Icon } from "@iconify/react";
+import { useSelector, useDispatch } from "react-redux";
+import { updateTotalPrice } from "../../stores/mySlice";
+import TableBody from "../../components/checkout/table";
+import { useRouter } from "next/router";
+import useStore from "../../stores/zustandStore";
 
 const Cart = () => {
-  const userId = useStore( state => state.userId );
+  const userId = useStore((state) => state.userId);
   const dispatch = useDispatch();
   const router = useRouter();
-  const cartList = useSelector(state => state.mySlice.cart);
+  const cartList = useSelector((state) => state.mySlice.cart);
 
-  const totalPrice = useSelector(state => state.mySlice.totalCartPrice);
+  const totalPrice = useSelector((state) => state.mySlice.totalCartPrice);
 
   const processCart = async () => {
     let list = [];
-    cartList.forEach(item => {
-        list.push({...item });
-      })
+    cartList.forEach((item) => {
+      list.push({ ...item });
+    });
 
-      list.forEach(item => {
-        item.user_id = userId;
-        item.product_id = item.id;
-      });
-    
-    console.log({list});
-      await fetch('/api/addToCart',{
-        method: "POST",
-        body: JSON.stringify(list)
-      }).then( val => val.json()).then(val => {
+    list.forEach((item) => {
+      item.user_id = userId;
+      item.product_id = item.id;
+    });
+
+    console.log({ list });
+    await fetch("/api/addToCart", {
+      method: "POST",
+      body: JSON.stringify(list),
+    })
+      .then((val) => val.json())
+      .then((val) => {
         // if(val){
-          // router.push('/shop/orders');
+        // router.push('/shop/orders');
         // }
-        console.log({val})
-      }).then(val =>  router.push('/shop/orders')).catch(error => console.log({error}));
-    
-  }
+        console.log({ val });
+      })
+      .then((val) => router.push("/shop/orders"))
+      .catch((error) => console.log({ error }));
+  };
 
   const totalCartPrice = () => {
     let total = 0;
-    cartList.forEach(item => {
+    cartList.forEach((item) => {
       total = item.price * item.quantity;
-    })
+    });
     return total;
-  }
-
+  };
 
   useEffect(() => {
-    dispatch(updateTotalPrice(totalCartPrice()))
-  })
+    dispatch(updateTotalPrice(totalCartPrice()));
+  });
 
-  useEffect(()=>{
-    if(!userId){
-      router.push('/accedi-registrati')
+  useEffect(() => {
+    if (!userId) {
+      router.push("/accedi-registrati");
     }
-  })
+  });
 
   return (
-    <div className=' table py-8'>
-      {cartList.length > 0 ? <div className="m-auto w-[95%] md:w-[90%] lg:w-2/3">
-        <section className='table py-1 '>
-          <table className="w-full">
-            <thead className='table_head'>
-              <tr>
-                <th> </th>
-                <th> </th>
-                <th>Prodotto</th>
-                <th>Prezzo</th>
-                <th>Quantita</th>
-                <th>Subtotale</th>
-              </tr>
-            </thead>
-            <tbody className=' '>
-              {cartList.map((item, index) => (
-                <TableBody
-                  key={index}
-                  id={item.id}
-                  name={item.name}
-                  price={item.price}
-                  quantity={item.quantity}
-                  item={item}
+    <div className=" table py-8">
+      {cartList.length > 0 ? (
+        <div className="m-auto w-[95%] md:w-[90%] lg:w-2/3">
+          <section className="table py-1 ">
+            <table className="w-full">
+              <thead className="table_head">
+                <tr>
+                  <th> </th>
+                  <th> </th>
+                  <th>Prodotto</th>
+                  <th>Prezzo</th>
+                  <th>Quantita</th>
+                  <th>Subtotale</th>
+                </tr>
+              </thead>
+              <tbody className=" ">
+                {cartList.map((item, index) => (
+                  <TableBody
+                    key={index}
+                    id={item.id}
+                    name={item.name}
+                    price={item.price}
+                    quantity={item.quantity}
+                    item={item}
+                  />
+                ))}
+              </tbody>
+            </table>
+          </section>
+          <section>
+            <div className=" border-b-2 border-[#ccc] border-solid pb-4">
+              <form className=" flex items-center justify-between content-center ml-auto text-sm  w-1/2 py-1">
+                <input
+                  className="border-2 bg-transparent border-[#ccc] border-solid rounded-3xl px-3 py-[0.2em] w-[75%]"
+                  type={"text"}
+                  placeholder={"CODICE PROMOZIONALE (COUPON)"}
                 />
-              ))}
+                <input
+                  className="rounded-3xl px-3 py-[0.3em] bg-black text-white"
+                  value={"APPLICA"}
+                  type={"submit"}
+                />
+              </form>
+            </div>
+            <div className="border-b-2 border-gray-400 border-solid text-sm py-3">
+              <div className="flex items-center justify-between w-1/2 ml-auto py-1">
+                <span>Subtotale</span>
+                <span className="text-red-500">€{totalCartPrice()}</span>
+              </div>
+              <div className="flex items-center justify-between w-1/2 ml-auto py-1">
+                <span>Sconto</span>
+                <span className="text-red-500">€0</span>
+              </div>
+              <div className="flex items-center justify-between w-1/2 ml-auto py-1">
+                <span>IVA (22%)</span>
+                <span className="text-red-500">€24.915</span>
+              </div>
+              <div className="flex items-center justify-between w-1/2 ml-auto py-1">
+                <span>Spedizione</span>
+                <span className="text-red-500">€0</span>
+              </div>
+            </div>
 
-            </tbody>
-          </table>
-        </section>
-        <section>
-          <div className=' border-b-2 border-[#ccc] border-solid pb-4'>
-            <form className=' flex items-center justify-between content-center ml-auto text-sm  w-1/2 py-1'>
-              <input
-                className='border-2 bg-transparent border-[#ccc] border-solid rounded-3xl px-3 py-[0.2em] w-[75%]'
-                type={'text'}
-                placeholder={'CODICE PROMOZIONALE (COUPON)'}
-              />
-              <input
-                className='rounded-3xl px-3 py-[0.3em] bg-black text-white'
-                value={'APPLICA'}
-                type={'submit'} />
-            </form>
-          </div>
-          <div className='border-b-2 border-gray-400 border-solid text-sm py-3'>
-            <div className='flex items-center justify-between w-1/2 ml-auto py-1'>
-              <span>Subtotale</span>
-              <span className='text-red-500'>€{totalCartPrice()}</span>
+            <div className="border-b-2 border-gray-400 border-solid py-3">
+              <div className="flex items-center justify-between w-1/2 ml-auto py-1">
+                <span>Totale</span>
+                <span className="text-red-500">€{totalCartPrice()}</span>
+              </div>
             </div>
-            <div className='flex items-center justify-between w-1/2 ml-auto py-1'>
-              <span>Sconto</span>
-              <span className='text-red-500'>€0</span>
-            </div>
-            <div className='flex items-center justify-between w-1/2 ml-auto py-1'>
-              <span>IVA (22%)</span>
-              <span className='text-red-500'>€24.915</span>
-            </div>
-            <div className='flex items-center justify-between w-1/2 ml-auto py-1'>
-              <span>Spedizione</span>
-              <span className='text-red-500'>€0</span>
-            </div>
-          </div>
 
-          <div className='border-b-2 border-gray-400 border-solid py-3'>
-            <div className='flex items-center justify-between w-1/2 ml-auto py-1'>
-              <span>Totale</span>
-              <span className='text-red-500'>€{totalCartPrice()}</span>
+            <div
+              className=" py-4 text-sm text-right"
+              onClick={(e) => {
+                console.log("pushing"), processCart();
+              }}
+            >
+              <span
+                style={{
+                  cursor: "pointer",
+                }}
+                className="bg-black text-white
+            px-3 py-1 rounded-3xl"
+              >
+                PROCEDI CON L'
+              </span>
             </div>
-          </div>
-
-          <div className=' py-4 text-sm text-right' onClick={(e) => {console.log("pushing"), processCart() }}>
-            <span style={{
-              cursor: "pointer"
-            }} className='bg-black text-white
-            px-3 py-1 rounded-3xl'>PROCEDI CON L'</span>
-          </div>
-        </section>
-      </div> :
-        <div className='m-auto w-[95%] md:w-[90%] lg:w-2/3'>
-          <div className='flex items-center justify-center'>
-            <div className='text-center '>
-              <h1 className='text-2xl font-bold'>Il tuo carrello è vuoto</h1>
-              <Link href='/'>
-                <a className='text-blue-500 flex items-center justify-between w-fit py-5 m-auto'>
-                  <Icon className='text-blue-500' icon="carbon:arrow-left" />
-                  <span className='text-blue-500'>Torna alla home</span>
+          </section>
+        </div>
+      ) : (
+        <div className="m-auto w-[95%] md:w-[90%] lg:w-2/3">
+          <div className="flex items-center justify-center">
+            <div className="text-center ">
+              <h1 className="text-2xl font-bold">Il tuo carrello è vuoto</h1>
+              <Link href="/">
+                <a className="text-blue-500 flex items-center justify-between w-fit py-5 m-auto">
+                  <Icon className="text-blue-500" icon="carbon:arrow-left" />
+                  <span className="text-blue-500">Torna alla home</span>
                 </a>
               </Link>
             </div>
           </div>
-        </div>}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Cart;
-
 
 //Carrello vuoto
