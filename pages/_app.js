8f5c1a4cd7eea78429704c01f07db0b0f72b67ce
Desmond <desezo@gmail.com>
2022-07-18 12:49:40 +0100

    try

--- pages/_app.js
+++ pages/_app.js
@@ -18,6 +18,12 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
         <Head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+          crosOrigin="anonymous"
+        />
         <title>MCS</title>
         </Head>
       <Script
