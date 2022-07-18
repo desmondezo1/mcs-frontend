051db6342e7af29e5982624f7cf21e0b56bd1d39
Desmond <desezo@gmail.com>
2022-07-18 10:19:59 +0100

    Revert "f"
    This reverts commit baf6a5837264d50464a0091e0faf1fdaf454acd4.

--- pages/api/payment/success.js
+++ pages/api/payment/success.js
@@ -19,6 +19,7 @@ export default async function handler(req, res) {
                 }   
               ).then(re => { re.json()}).then(re => {
                 if (re.status == 200) {
+                  
                   return {
                      redirect: {
                         destination: '/shop?m=success',
