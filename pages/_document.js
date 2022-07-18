783ae54648171595884a3225018842a4735b213d
Desmond <desezo@gmail.com>
2022-07-18 12:47:06 +0100

    test

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ export default function Document() {
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
           rel="stylesheet"
           integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-          crosOrigin="anonymous"
+          crossOrigin="true"
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
@@ -30,6 +30,7 @@ export default function Document() {
         <link
           href="https://fonts.googleapis.com/css2?family=Almarai:wght@300;400;700;800&display=swap"
           rel="stylesheet"
+          crossOrigin="true"
         />
         <link
           rel="apple-touch-icon"
