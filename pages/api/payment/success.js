0e34812592582876efb593d638dca44e54a53f70
Desmond <desezo@gmail.com>
2022-07-18 09:45:43 +0100

    ....

--- pages/api/payment/success.js
+++ pages/api/payment/success.js
@@ -19,7 +19,6 @@ export default async function handler(req, res) {
                 }   
               ).then(re => { re.json()}).then(re => {
                 if (re.status == 200) {
-                  
                   return {
                      redirect: {
                         destination: '/shop?m=success',
